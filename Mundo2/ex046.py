00476689b4b6eb3271f1b26fa8e988228a5628ea
Guilherme Ramiro <guizzhaleixo@gmail.com>
2026-07-27 10:33:43 -0300

    Exercício 46: Contagem regressiva

---
+++ Mundo2/ex046.py
@@ -0,0 +1,7 @@
+# CONTAGEM REGRESSIVA # 
+import time 
+print("CONTAGEM REGRESSIVA PARA OS FOGOS: ")
+for c in range(10, -1, -1):
+    print(c) 
+    time.sleep(1) 
+print("FOGOS ESTOURANDO!! POW POW POW")
