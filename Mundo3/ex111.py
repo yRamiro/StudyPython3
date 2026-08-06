fcc10bdd1876fe2d75b949249c3f86fbe3ec09d0
Guilherme Ramiro <guizzhaleixo@gmail.com>
2026-08-06 10:14:21 -0300

    Exercício 111: Transformando módulos em pacotes

---
+++ Mundo3/ex111.py
@@ -0,0 +1,4 @@
+# TRANSFORMANDO MÓDULOS EM PACOTES #
+# -> O EXERCÍCIO 111 FOI CRIAR O PACOTE 'utilidadesCeV' 
+# Muito provavelmente, nas commits não apareça. Mas antes do pacote 'utilidadesCeV' os dados estavam no módulo 'moedas.py' e todas as funções eram extraídas de lá com 'import moeda' 
+import utilidadesCeV
